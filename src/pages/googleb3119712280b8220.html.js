dd820deba47defb93edd64bbeee343053b0f277b
Martin Shishkov <martin.shishkov@hotmail.com>
2018-04-04 21:24:30 +0300

    added seach console

---
+++ src/pages/googleb3119712280b8220.html.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+export default class SearchConsolePage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        return (
+            <div>
+                google-site-verification: googleb3119712280b8220.html
+            </div>
+        );
+    }
+}
